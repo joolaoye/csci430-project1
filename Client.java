c0f176c3ff0ed9b0e938483e0675f53621a646d7
CarsonStallcup <115600034+CarsonStallcup@users.noreply.github.com>
2025-09-29 18:53:10 -0500

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,83 @@
+// Name: Carson Stallcup
+// Group 3
+// StarID: po1278cm
+// File: Client.java
+// Purpose: This class contains all information pertaining to each user. 
+
+
+public class Client {
+    private String id;
+    private String name;
+    private String address;
+    private double balance;
+
+
+    //Initializing.
+    public Client(String id, String name, String address, double balance) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.balance = balance;
+    }
+
+
+    public Client(String id, String name, String address) {
+        this(id, name, address, 0.0);
+    }
+
+
+    //GET Functions
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getBalance() {
+
+        return balance;
+    }
+
+
+
+
+    //SET Functions
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void updateBalance(double amount) {
+        this.balance += amount;
+    }
+
+
+
+
+    //Relationship to wish lists (placeholder)
+    public List<Wishlist> getWishlists() {
+        return new ArrayList<>(wishlists);
+    }
+
+
+    //Relationship to transactions (placeholder)
+    public List<Transaction> getTransactions() {
+        return new ArrayList<>(transactions);
+    }
+
+    //Information display
+    @Override
+    public String toString() {
+        return String.format("Client[%s] %s, %s, Balance: $%.2f",
+                id, name, address, balance);
+    }
+}
---
+++ ClientList.java
@@ -0,0 +1,113 @@
+// Name: Carson Stallcup
+// StarID: po1278cm
+// Group 3
+// Purpose: This class contains many clients.  It can get, 
+// add, or delete clients and modify the list.
+
+import java.util.*;
+
+public class ClientList {
+    private Map<String, Client> clients;
+
+    public ClientList() {
+        this.clients = new HashMap<>();
+    }
+
+
+    //Adding a client to the list.
+    public Client addClient(String name, String address) {
+        String uniqueId = UUID.randomUUID().toString();
+        Client client = new Client(uniqueId, name, address, 0.0);
+        clients.put(uniqueId, client);
+        return client;
+    }
+
+
+
+
+
+
+
+    Used to delete a particular client.
+    public void deleteClient(String clientId) {
+        if (!clients.containsKey(clientId)) {
+            throw new NoSuchElementException("No client with ID " + clientId + " found.");
+        }
+        clients.remove(clientId);
+    }
+
+
+
+
+
+
+
+
+
+
+    Used to get a particular client.
+    public Client getClient(String clientId) {
+        return clients.get(clientId);
+    }
+    
+
+    Used to display all clients.
+    public List<Client> listClients() {
+        return new ArrayList<>(clients.values());
+    }
+
+
+
+
+
+
+
+
+
+
+    //Searching fir a client by their name.
+    public List<Client> searchByName(String name) {
+        List<Client> result = new ArrayList<>();
+        for (Client c : clients.values()) {
+            if (c.getName().equalsIgnoreCase(name)) {
+                result.add(c);
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //Gets transactions through the client and shows all transactions a client has ever conducted.
+    public List<Transaction> getAllTransactions() {
+        List<Transaction> allTransactions = new ArrayList<>();
+        for (Client c : clients.values()) {
+            allTransactions.addAll(c.getTransactions());
+        }
+        return allTransactions;
+    }
+
+
+
+
+
+
+
+
+    //Displays all items on a client's wish list based on the relationship between the client and their wish list.
+    public List<Wishlist> getAllWishlists() {
+        List<Wishlist> allWishlists = new ArrayList<>();
+        for (Client c : clients.values()) {
+            allWishlists.addAll(c.getWishlists());
+        }
+        return allWishlists;
+    }
+}
